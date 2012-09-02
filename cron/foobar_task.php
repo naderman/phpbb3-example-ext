133e6dd1d0f76a803745dede7b5602869be064bc
Igor Wiedler <igor@wiedler.ch>
2012-09-03 00:07:01 +0200

    Update cron task to use the DIC

--- cron/foobar_task.php
+++ cron/foobar_task.php
@@ -2,10 +2,21 @@
 
 class phpbb_ext_naderman_example_cron_foobar_task extends phpbb_cron_task_base
 {
+    private $phpbb_root_path;
+    private $user;
+
+    public function __construct($phpbb_root_path, phpbb_user $user)
+    {
+        $this->phpbb_root_path = $phpbb_root_path;
+        $this->user = $user;
+    }
+
     public function run()
     {
-        global $phpbb_root_path, $user;
-        $user->add_lang_ext('foobar', 'foobar');
-        file_put_contents($phpbb_root_path.'/foobar.txt', $user->lang('FOOBAR', date('s')));
+        $this->user->add_lang_ext('naderman/example', 'foobar');
+
+        $filename   = $this->phpbb_root_path.'/foobar.txt';
+        $content    = $this->user->lang('FOOBAR', date('s'));
+        file_put_contents($filename, $content);
     }
 }
