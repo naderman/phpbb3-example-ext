13b4c0552513e93e6b2dd80ba4085fee30a99d75
David King <imkingdavid@gmail.com>
2013-01-07 12:48:55 -0500

    Further updates to make this compatible with develop

--- controller/main.php
+++ controller/main.php
@@ -10,10 +10,10 @@ class phpbb_ext_naderman_example_controller_main
 	* @param phpbb_request $request Request object
 	* @param phpbb_user $user User object
 	* @param phpbb_template $template Template object
-	* @param dbal $db DBAL object
+	* @param phpbb_db_driver $db Database object
 	* @param phpbb_controller_helper $helper Controller helper object
 	*/
-	public function __construct(phpbb_request $request, phpbb_user $user, phpbb_template $template, dbal $db, phpbb_controller_helper $helper)
+	public function __construct(phpbb_request $request, phpbb_user $user, phpbb_template $template, phpbb_db_driver $db, phpbb_controller_helper $helper)
 	{
 		$this->request = $request;
 		$this->user = $user;
@@ -33,7 +33,7 @@ class phpbb_ext_naderman_example_controller_main
 	{
 		if (empty($test))
 		{
-			return $this->helper->error(404, 'foo() method called, but no value was given for $test. A 404, "Not Found", response was sent.');
+			return $this->helper->error('foo() method called, but no value was given for $test. A 404, "Not Found", response was sent.', 404);
 		}
 
 		$this->template->assign_vars(array(
@@ -44,13 +44,13 @@ class phpbb_ext_naderman_example_controller_main
 	}
 
 	/**
-	* Foo controller to be accessed with the URL is /ex
+	* Foo controller to be accessed with the URL is /ex/{test}
 	* (where {test} is the placeholder for a value)
 	*
 	* @param string $test String taken from the URL
 	* @return Symfony\Component\HttpFoundation\Response A Symfony Response object
 	*/
-	public function bar($test = bar)
+	public function bar($test = 'bar')
 	{
 		$this->user->add_lang_ext('naderman/example', 'foobar');
 
